770f50c89a2ee2d161dcf46562ec24041db7491c
jwillikers <19399197+jwillikers@users.noreply.github.com>
2026-08-24 01:13:17 +0000

    chore(deps): Update Nix packages

--- packages/icinga-container-entrypoint/package.nix
+++ packages/icinga-container-entrypoint/package.nix
@@ -1,17 +1,17 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "icinga-container-entrypoint";
-  version = "0-unstable-2026-06-25";
+  version = "0-unstable-2026-08-17";
 
   src = fetchFromGitHub {
     owner = "Icinga";
     repo = "docker-icinga2";
-    rev = "e059eccf16fcbb6b36079ea54158a0341765ce06";
-    sha256 = "sha256-zl3KeRd4VYOsRUyKEth0H7pAvTCnJ51A8sNk/+ZE4aY=";
+    rev = "71fe89f8d30b944d11a835022383aae1dbd14ee6";
+    sha256 = "sha256-gsX4n/CkCvJ4Qpg9y/p0yO//Ri3VsU0F3ST6ysh4BAo=";
   };
   sourceRoot = "${src.name}/entrypoint";
 
-  vendorHash = "sha256-loIOutu+1l6u3UfxdTQQIX45NREaZxfrGaByktgKngE=";
+  vendorHash = "sha256-Fu5Cax5W/Ozzg5+RVNxOebH4x4C0c2yfLUkH4VN1AJE=";
 
   meta = {
     mainProgram = "entrypoint";
